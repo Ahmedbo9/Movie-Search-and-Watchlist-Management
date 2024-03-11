72de2350c510e75f27a48e7081e6c3e4db8d96a0
Ahmedbo9 <ahmed.benothman@live.fr>
2024-03-10 23:07:21 -0400

    adding to watch list

--- github-prime-video/src/App.js
+++ github-prime-video/src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 import MovieList from './components/movies/movie_list'
 import Header from './components/movies/header'
 import SearchBar from './components/search_bar';
+import Favourite from './components/movies/favourite';
 
 const App = () => {
   const [movies, setMovies] = useState([])
-
   const [searchValue, setSearchValue] = useState('')
+  const [toWatchList, setToWatchList] = useState([])
 
   const getMovies = async (searchValue) => {
     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=e05621ed`
@@ -21,6 +22,11 @@ const App = () => {
 
   useEffect(() => {getMovies(searchValue);},[searchValue])
 
+  const addToWatchList = (movie) => {
+    const newWatchList = [...toWatchList, movie];
+    setToWatchList(newWatchList);
+  }
+
 
   return (
     <div className='container-fluid grid'>
@@ -32,9 +38,21 @@ const App = () => {
     </div>
 
     <div className='row'>
-      <MovieList movies={movies} />
+      <MovieList movies={movies} movieOnClick ={addToWatchList} favourite = {Favourite} />
     </div>
+
+
+    <div className='row d-flex align-item-center mt-4 mb-4'>
+      <Header title ='To watch later'/>
+    </div>
+
+    <div className='row'>
+      <MovieList movies={toWatchList} movieOnClick ={addToWatchList} favourite = {Favourite} />
+    </div>
+
   </div>
+
+  
   
   )
 }
--- github-prime-video/src/components/movies/movie_list.js
+++ github-prime-video/src/components/movies/movie_list.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import Favourite from './favourite';
 
 const MovieList = (props) => {
+    const Favourite = props.favourite;
     return (
         <>
             {props.movies.map((movie, index) => {
                 return (
-                    <div className='d-flex justify-content-start m-3' key={index}>
+                    <div className='image-container d-flex justify-content-start m-3' key={index}>
                         <img src={movie.Poster} alt="movie poster"/>
+                        <div onClick={ () =>props.movieOnClick(movie) } className='overlay d-flex align-items-center justify-conten-center'>
+                        <Favourite/>
+                        </div>
                     </div>
                 )
             })}
